b15cc33591fe80e62052025ce0b6b0a06552a746
LorenzoMattia <63303421+LorenzoMattia@users.noreply.github.com>
2020-06-16 11:51:10 +0200

    Aggiunta classe tag e relative associazioni

--- taskmanager_2/src/main/java/it/uniroma3/siw/model/Task.java
+++ taskmanager_2/src/main/java/it/uniroma3/siw/model/Task.java
@@ -1,11 +1,13 @@
 package it.uniroma3.siw.model;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 
@@ -21,6 +23,9 @@ public class Task {
 	boolean completed;
 	LocalDateTime creationTimeStamp;
 	LocalDateTime lastUpdateTimeStamp;
+	
+	@ManyToMany
+	List<Tag> tags;
 	
 	public Long getId() {
 		return id;
